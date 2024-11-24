f566dd2e81c19c8a3daa135394c1ec23a76968f5
JoãoV <168022398+cleitao-anao@users.noreply.github.com>
2024-11-24 22:11:11 +0000

    Carrinho melhorado

--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useStore } from '../store'; // Certifique-se de ajustar o caminho
 import './Carrinho.css';
 
@@ -5,25 +6,30 @@ const Carrinho = () => {
   const { products, totalItems, totalPrice, addToCart, removeFromCart, clearCart } = useStore();
 
   return (
-    <div>
+    <div className="cart-container">
       <h1>Carrinho de Compras</h1>
       {products.length === 0 ? (
         <p>O carrinho está vazio.</p>
       ) : (
         <ul>
-          {products.map((product) => (
-            <li key={product.id}>
-              <img src={product.image} alt={product.name} style={{ width: '50px' }} />
-              {product.name} - {product.quantity}x R${product.price.toFixed(2)}
-              <button onClick={() => addToCart(product)}>Adicionar</button>
-              <button onClick={() => removeFromCart(product.id)}>Remover</button>
-            </li>
-          ))}
-        </ul>
+  {products.map((product) => (
+    <li key={product.id}>
+      <img src={product.image} alt={product.name} style={{ width: '50px' }} />
+      <div>
+      {product.name} - {product.quantity}x R${(product.new_price || 0).toFixed(2)}
+      </div>
+      <button onClick={() => addToCart(product)}>Adicionar</button>
+      <button onClick={() => removeFromCart(product.id)}>Remover</button>
+    </li>
+  ))}
+</ul>
+
       )}
       <p>Total de itens: {totalItems()}</p>
       <p>Preço total: R${totalPrice().toFixed(2)}</p>
-      <button onClick={clearCart}>Esvaziar Carrinho</button>
+      <button className="clear-cart" onClick={clearCart}>
+        Esvaziar Carrinho
+      </button>
     </div>
   );
 };
